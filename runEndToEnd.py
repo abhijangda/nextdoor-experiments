9bf9c919c8127954db207db63a8380708786b64f
Abhinav Jangda <abhijangda@gmail.com>
2021-03-05 10:28:35 -0500

    end to end run

--- runEndToEnd.py
+++ runEndToEnd.py
@@ -1,5 +1,8 @@
 import argparse, os, subprocess
 import shutil, re
+import datetime 
+
+logFile = os.path.join(os.getcwd(), "end2endBenchmarking.log")
 
 parser = argparse.ArgumentParser(description='Benchmark')
 parser.add_argument('-nextdoor', type=str,
@@ -14,22 +17,39 @@ graphInfo = {
     # "LiveJournal": {"v": 4847569, "path": os.path.join(input_dir, "LJ1.data")},
     # "Orkut": {"v":3072441,"path":os.path.join(input_dir, "orkut.data")},
     # "Patents": {"v":6009555,"path":os.path.join(input_dir, "patents.data")},
-    # "Reddit": {"v":232965,"path":os.path.join(input_dir, "reddit.data")}
+    "Reddit": {"v":232965,"path":os.path.join(input_dir, "reddit.data")}
 }
+
+def writeToLog(s):
+    if not os.path.exists(logFile):
+        open(logFile,"w").close()
+    
+    f = open(logFile, "r+")
+    f.write(s)
+    f.close()
+
+writeToLog("=========Starting Run at %s=========="%(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+
 
 gnns = ['FastGCN', 'LADIES']
 #Build sampling application in NextDoor folder
-# for gnn in gnns:
-#   d = os.path.join(args.nextdoor,'src/apps/',gnn.lower())
-#   print(d)
-#   os.chdir(d)
-#   status, output = subprocess.getstatusoutput("make clean")
-#   status, output = subprocess.getstatusoutput("make -j")
-#   print (output)
-#   #Copy libraries from NextDoor folder to GNN folder
-#   src = os.path.join(d, gnn+"SamplingPy3.so")
-#   dst = os.path.join(cwd, 'LADIES/',gnn+"SamplingPy3.so")
-#   shutil.copyfile(src, dst)
+for gnn in gnns:
+  d = os.path.join(args.nextdoor,'src/apps/',gnn.lower())
+  os.chdir(d)
+  writeToLog("Chdir to "+ d)
+  writeToLog("Executing make")
+  status, output = subprocess.getstatusoutput("make clean")
+  if (status != 0):
+    print(output)
+  writeToLog(output)
+  status, output = subprocess.getstatusoutput("make -j")
+  if (status != 0):
+    print(output)
+  writeToLog(output)
+  #Copy libraries from NextDoor folder to GNN folder
+  src = os.path.join(d, gnn+"SamplingPy3.so")
+  dst = os.path.join(cwd, 'LADIES/',gnn+"SamplingPy3.so")
+  shutil.copyfile(src, dst)
 
 os.chdir(cwd)
 baselineResults = {gnn.lower(): {graph: -1 for graph in graphInfo} for gnn in gnns}
@@ -37,26 +57,40 @@ nextdoorResults = {gnn.lower(): {graph: -1 for graph in graphInfo} for gnn in gn
 
 def runForGNN(gnn):
   global results
+  
   if (gnn == 'FastGCN' or gnn == 'LADIES'):
     os.chdir('./LADIES')
     gnnCommand = "python3 pytorch_ladies.py --cuda 0 --dataset %s --sample_method fastgcn --epoch_num 10 --n_iters 2"
-
+  writeToLog("doing perf eval of %s"%gnn)
   status,output = subprocess.getstatusoutput("env -i bash -c 'source venv/bin/activate && env'")
+  writeToLog(output)
   for line in output.split('\n'):
     (key, _, value) = line.partition("=")
     os.environ[key] = value
     
   for graph in graphInfo:
     c = gnnCommand % graph.lower()
+    print(c)
+    writeToLog("executing " + c)
     status,output = subprocess.getstatusoutput(c)
+    writeToLog(output)
     samplerTimes = re.findall('end_to_end_time.+', output)
     for samplerTime in samplerTimes:
       s = re.findall(r'\((\w+)\)\s*([\.\d]+)', samplerTime)[0]
       samplerName = s[0]
       time = s[1]
       if 'nextdoor_' in samplerName:
-        nextdoorResults[samplerName[len("nextdoor_"):]] = time
+        nextdoorResults[samplerName[len("nextdoor_"):]][graph] = float(time)
       else:
-        baselineResults[samplerName] = time
+        baselineResults[samplerName][graph] = float(time)
 
 runForGNN('FastGCN')
+ 
+#Print results
+print ("\n\nTable 5: End-to-end speedups after integratingcuSamplerin GNNs over vanilla GNNs")
+row_format = "{:>30}" * 3
+print (row_format.format("GNN", "Graph", "Speedup"))
+for samplerName in baselineResults:
+    for graph in graphInfo:
+        speedup = baselineResults[samplerName][graph]/nextdoorResults[samplerName][graph]
+        print (row_format.format(samplerName, graph, speedup))
