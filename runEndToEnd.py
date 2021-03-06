f5ebbb869e3a8a7fab8714c90900ef221eda044c
Your Name <you@example.com>
2021-03-06 21:34:39 +0000

    runEndToEnd on all graphs

--- runEndToEnd.py
+++ runEndToEnd.py
@@ -15,9 +15,9 @@ input_dir = os.path.join(args.nextdoor, 'input')
 
 graphInfo = {
     "PPI": {"v": 56944, "path": os.path.join(input_dir, "ppi.data")},
-    # "LiveJournal": {"v": 4847569, "path": os.path.join(input_dir, "LJ1.data")},
-    # "Orkut": {"v":3072441,"path":os.path.join(input_dir, "orkut.data")},
-    # "Patents": {"v":6009555,"path":os.path.join(input_dir, "patents.data")},
+    "LiveJournal": {"v": 4847569, "path": os.path.join(input_dir, "LJ1.data")},
+    "Orkut": {"v":3072441,"path":os.path.join(input_dir, "orkut.data")},
+    "Patents": {"v":6009555,"path":os.path.join(input_dir, "patents.data")},
     "Reddit": {"v":232965,"path":os.path.join(input_dir, "reddit.data")}
 }
 
